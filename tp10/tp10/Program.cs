527d0c8c3bb21f180b73ce71f053c959e4336848
Chicabytes <62666993+Chicabytes@users.noreply.github.com>
2020-07-12 20:31:28 -0300

    pto1

---
+++ tp10/tp10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace tp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NombreArchivo = "ListadoInmuebles.csv";
+            string PathArch = @"C:\Users\giuli\Documents\Facultad\Taller1\TP10\tp10\tp10\";
+            List<Propiedad> ListaPropiedades = new List<Propiedad>();
+            FileStream Arch_Leer = new FileStream(PathArch + NombreArchivo, FileMode.Open);
+            StreamReader Leer = new StreamReader(Arch_Leer);
+
+            string Linea;
+            int cont = 0;
+            while ((Linea = Leer.ReadLine()) != null)
+            {
+                int id = cont;
+                string[] Dato = Linea.Split(';');
+                string Doc = Dato[0];
+                string TipoProp = Dato[1];
+                Propiedad Prop = new Propiedad();
+                Prop.CargarInmueble(id, Doc, TipoProp);
+                ListaPropiedades.Add(Prop);
+                cont++;
+            }
+            Leer.Close();
+
+            string NuevoArch = "ListadoInmueblesCompleto.csv";
+            FileStream Arch_Escribir = new FileStream(PathArch + NuevoArch, FileMode.OpenOrCreate);
+            StreamWriter Escribir = new StreamWriter(Arch_Escribir);
+            foreach(Propiedad _prop in ListaPropiedades)
+            {
+                Escribir.WriteLine(_prop.Id + ';' + _prop.Tamanio + ';' + _prop.Cantidad_baños + ';' + _prop.Cantidad_habitaciones + ';' + _prop.Domicilio + ';' + _prop.Operacion + ';' + _prop.Precio + ';' + _prop.Estado + ';' + _prop.Propiedades + ';');
+                Console.WriteLine(_prop.Id + ';' + _prop.Tamanio + ';' + _prop.Cantidad_baños + ';' + _prop.Cantidad_habitaciones + ';' + _prop.Domicilio + ';' + _prop.Operacion + ';' + _prop.Precio + ';' + _prop.Estado + ';' + _prop.Propiedades + ';');
+
+            }
+            Escribir.Close();
+        }
+    }
+}
---
+++ tp10/tp10/Propiedad.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tp10
+{
+    public class Propiedad
+    {
+
+        enum TipoDeOperacion
+        {
+            Venta,
+            Alquiler
+        }
+        enum TipoDePropiedad
+        {
+            Departamento,
+            Casa,
+            Duplex,
+            Penthouse,
+            Terreno
+        }
+
+        private int id;//representa el número de propiedad ingresada
+        private float tamanio; // punto flotante
+        private int cantidad_baños;
+        private int cantidad_habitaciones;
+        private string domicilio; // string
+        private int precio; // Valor Entero
+        private bool estado = false; // bool activo / inactivo
+        private string propiedades;
+        private string operacion;
+
+        public int Id { get => id; set => id = value; }
+        public float Tamanio { get => tamanio; set => tamanio = value; }
+        public int Cantidad_baños { get => cantidad_baños; set => cantidad_baños = value; }
+        public int Cantidad_habitaciones { get => cantidad_habitaciones; set => cantidad_habitaciones = value; }
+        public string Domicilio { get => domicilio; set => domicilio = value; }
+        public int Precio { get => precio; set => precio = value; }
+        public bool Estado { get => estado; set => estado = value; }
+        public string Propiedades { get => propiedades; set => propiedades = value; }
+        public string Operacion { get => operacion; set => operacion = value; }
+
+        public void CargarInmueble(int _Id, string _Domicilio, string _Propiedades)
+        {
+            Random Rand = new Random();
+            Id = _Id;
+            Tamanio = Convert.ToSingle(Rand.Next());
+            Cantidad_baños = Rand.Next(1,5);
+            Cantidad_habitaciones = Rand.Next(1,10);
+            Domicilio = _Domicilio;
+            TipoOp();
+            Precio = Rand.Next(10000, 1000000);
+            Estado = Convert.ToBoolean(Rand.Next(1));
+            Propiedades = _Propiedades;
+        }
+
+        public float ValorDelInmueble()// sólo devuelve un valor
+        {
+            float Precio_Operacion;
+            if (TipoDeOperacion.Venta.ToString() == Operacion)
+            {
+                Precio_Operacion = Convert.ToSingle(Precio + (Precio * 0.21) + (Precio * 0.10) + 10000);
+                return Precio_Operacion;
+            }
+            else
+            {
+                Precio_Operacion = Convert.ToSingle((Precio*0.02) + (Precio*0.005));
+                return Precio_Operacion;
+            }
+        }
+
+        public void TipoOp()
+        {
+            Random rand = new Random();
+            int v_rand = rand.Next(100);
+            string[] result;
+            result = Enum.GetNames(typeof(TipoDeOperacion));
+            if (v_rand > 65)
+            { 
+                Operacion = result[0];
+            }
+            else
+            {
+                Operacion = result[1];
+            }
+        }
+    }
+}
